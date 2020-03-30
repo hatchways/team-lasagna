cd1eda8259202a003da3a02d613ab2e1b623eab2
Din Lanh <langdinhieu@gmail.com>
2020-03-30 14:58:29 -0400

    added trim and default to profile model

--- server/models/Profile.js
+++ server/models/Profile.js
@@ -3,21 +3,31 @@ const mongoose = require('mongoose');
 const profileSchema = new mongoose.Schema({
   firstName: {
     type: String,
+    trim: true,
+    default: ""
   },
   lastName: {
     type: String,
+    trim: true,
+    default: ""
   },
   gender: {
+    trim: true,
     type: String,
+    default: ""
   },
   birthDate: {
     type: Date,
   },
   description: {
     type: String,
+    trim: true,
+    default: ""
   },
   availability: {
     type: String,
+    trim: true,
+    default: ""
   },
   user: {
     type: mongoose.Schema.Types.ObjectId,
