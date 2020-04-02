47b2b157140f4f21244f8d87142216ab99c17f38
xiezihen <frank.xie@mail.utoronto.ca>
2020-04-01 22:49:49 -0400

    finished authenticate user for login endpoint

--- server/controllers/loginController.js
+++ server/controllers/loginController.js
@@ -8,10 +8,19 @@ module.exports.authenticateUser = async (req, res) => {
   }
   try {
     const user = await User.findOne({ email: email });
-    if (!user){
-        return res.status(400).send({msg : "User not found"})
+    if (!user) {
+      return res.status(400).send({ msg: "User not found" });
     }
-    console.log(user);
+    //console.log(user);
+    isMatch = await User.comparePassword(password, user.password);
+    if (isMatch) {
+      const token = jwt.sign(user.toJSON(), process.env.SECRET, {
+        expiresIn: 2000
+      });
+      return res.status(200).send({ token: token });
+    }
+    return res.status(400).send({ msg: "Email and password dont match" });
+    console.log(isMatch);
   } catch (err) {
     res.status(500).send({ msg: "server error" });
   }
