54c4e73c52b342456932664d6c4ee27fdd9e6d8e
Din Lanh <langdinhieu@gmail.com>
2020-04-02 14:31:26 -0400

    added image upload service

---
+++ server/services/image-upload.js
@@ -0,0 +1,39 @@
+const aws = require("aws-sdk");
+const multer = require("multer");
+const multerS3 = require("multer-s3");
+
+//const config = require("../config");
+
+aws.config.update({
+  secretAccessKey: "",
+  accessKeyId: "",
+  region: "us-east-1"
+});
+
+const s3 = new aws.S3();
+
+const fileFilter = (req, file, cb) => {
+  if (file.mimetype === "image/jpeg" || file.mimetype === "image/png") {
+    cb(null, true);
+  } else {
+    cb(new Error("Invalid Mime Type, only JPEG and PNG"), false);
+  }
+};
+
+const upload = multer({
+  fileFilter,
+  storage: multerS3({
+    s3,
+    bucket: "pet-sitter-imgs",
+    acl: "public-read",
+    contentType: multerS3.AUTO_CONTENT_TYPE,
+    metadata: (req, file, cb) => {
+      cb(null, { fieldName: "Meta-data" });
+    },
+    key: (req, file, cb) => {
+      cb(null, file.originalname);
+    }
+  })
+});
+
+module.exports = upload;
