a0797b14b8ec6ace8753b2d591ec43dca65629ca
Din Lanh <langdinhieu@gmail.com>
2020-04-02 11:10:52 -0400

    added fName lName validations

--- server/utils/profileValidation.js
+++ server/utils/profileValidation.js
@@ -1,11 +1,17 @@
 const { check } = require("express-validator");
 
 const validate = [
+  check("firstName")
+    .exists()
+    .matches(/^[a-zA-Z]+$/),
+  check("lastName")
+    .exists()
+    .matches(/^[a-zA-Z]+$/),
   check("phone")
     .optional()
     .custom(value => {
       // false if does not pass
-      // simple validation for langth and 10 digits or empty string
+      // simple validation for length and 10 digits or empty string
       if ((value.length === 10 && value.match(/^\d{10}$/)) || value === "") {
         return true;
       } else {
