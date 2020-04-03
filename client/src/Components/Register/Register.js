079e3a101370ed245a0b070d8cdef80184aa340c
xiezihen <frank.xie@mail.utoronto.ca>
2020-04-03 03:08:50 -0400

    frontend register succcessfully posts to both register and profile

--- client/src/Components/Register/Register.js
+++ client/src/Components/Register/Register.js
@@ -2,16 +2,13 @@ import React from "react";
 import "./Register.css";
 import RegisterForm from "./RegisterForm/RegisterForm";
 
-class Register extends React.Component {
-  state = {};
-  render() {
-    return (
-      <div>
-        <h1>Register</h1>
-        <RegisterForm />
-      </div>
-    );
-  }
+function Register() {
+  return (
+    <div>
+      <h1>Register</h1>
+      <RegisterForm />
+    </div>
+  );
 }
 
 export default Register;
--- client/src/Components/Register/RegisterForm/RegisterForm.js
+++ client/src/Components/Register/RegisterForm/RegisterForm.js
@@ -1,44 +1,76 @@
 import React from "react";
+import axios from "axios";
 import "./RegisterForm.css";
+import { useForm } from "react-hook-form";
+import { RHFInput } from "react-hook-form-input";
 import { TextField, Button } from "@material-ui/core";
 
-class RegisterForm extends React.Component {
-  state = {};
-  render() {
-    return (
-      <form>
-        <div class="flex-container">
-          <TextField
-            id="email"
-            label="Your Email"
-            type="email"
-            variant="outlined"
-          ></TextField>
-          <TextField
-            id="firstName"
-            label="Your first name"
-            type="string"
-            variant="outlined"
-          ></TextField>
-          <TextField
-            id="lastName"
-            label="Your last name"
-            type="string"
-            variant="outlined"
-          ></TextField>
-          <TextField
-            id="email"
-            label="Create Password"
-            type="string"
-            variant="outlined"
-          ></TextField>
-        </div>
-        <Button variant="contained" color="red">
-          SIGN UP
-        </Button>
-      </form>
-    );
-  }
+function RegisterForm() {
+  const { register, handleSubmit, setValue, reset } = useForm();
+  const onSubmit = async data => {
+    const createUserRes = await axios.post("http://localhost:3001/register", {
+      email: data.email,
+      password: data.password
+    });
+    console.log(createUserRes.data._id);
+
+    if (createUserRes.status === 200) {
+      const createProfileRes = await axios.post(
+        "http://localhost:3001/profile",
+        {
+          firstName: data.firstName,
+          lastName: data.lastName,
+          user: createUserRes.data._id
+        }
+      );
+    }
+  };
+  //console.log(watch("example"));
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <div class="flex-container">
+        <RHFInput
+          as={<TextField type="email" />}
+          register={register}
+          setValue={setValue}
+          rules={{ required: true }}
+          name="email"
+          label="Your Email"
+          variant="outlined"
+        />
+        <RHFInput
+          as={<TextField />}
+          register={register}
+          setValue={setValue}
+          rules={{ required: true }}
+          name="firstName"
+          label="Your first name"
+          variant="outlined"
+        />
+        <RHFInput
+          as={<TextField />}
+          register={register}
+          setValue={setValue}
+          rules={{ required: true }}
+          name="lastName"
+          label="Your last name"
+          variant="outlined"
+        />
+        <RHFInput
+          as={<TextField type="password" />}
+          register={register}
+          setValue={setValue}
+          rules={{ required: true, min: 6 }}
+          name="password"
+          label="Create a password"
+          variant="outlined"
+        />
+      </div>
+      <Button variant="contained" color="red" type="submit">
+        SIGN UP
+      </Button>
+    </form>
+  );
 }
 
 export default RegisterForm;
