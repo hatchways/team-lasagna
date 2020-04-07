031e9a142f10f7db0479a7c8ccf237ecd6b1734d
Din Lanh <langdinhieu@gmail.com>
2020-04-07 13:25:20 -0400

    implemented image delete

--- server/controllers/imgUploadController.js
+++ server/controllers/imgUploadController.js
@@ -1,21 +1,21 @@
-const upload = require("../services/image-upload");
+const s3 = require("../services/image-upload");
 const Profile = require("../models/Profile");
-const uploadSingle = upload.single("image");
+const uploadSingle = s3.upload.single("image");
 
 module.exports.imgUpload = (req, res, err) => {
-  uploadSingle(req, res, async err => {
+  uploadSingle(req, res, async (err) => {
     if (err) {
       return res.status(422).send({
-        errors: [{ title: "File Upload Error", detail: err }]
+        errors: [{ title: "File Upload Error", detail: err }],
       });
     }
     const profile = await Profile.findByIdAndUpdate(
       req.params.userId,
       {
-        profilePic: req.file.location
+        profilePic: req.file.location,
       },
       {
-        new: true
+        new: true,
       }
     );
     if (!profile) {
@@ -23,4 +23,24 @@ module.exports.imgUpload = (req, res, err) => {
     }
     return res.status(200).json(profile);
   });
+};
+
+module.exports.imgDelete = async (req, res, err) => {
+  const profile = await Profile.findById(req.params.userId);
+  if (!profile) {
+    return res.status(404).json({ msg: "Profile not found" });
+  }
+  const split = profile.profilePic.split("/");
+  const key = decodeURIComponent(split[split.length - 1]);
+  s3.delete(key);
+  const updatedProfile = await Profile.findByIdAndUpdate(
+    req.params.userId,
+    {
+      profilePic: "",
+    },
+    {
+      new: true,
+    }
+  );
+  res.status(200).json(updatedProfile);
 };
