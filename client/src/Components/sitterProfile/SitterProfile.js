98ece6f5ddec0739483190abee9446f0d019b498
xiezihen <frank.xie@mail.utoronto.ca>
2020-04-09 06:21:06 -0400

    dog sitting profile

---
+++ client/src/Components/sitterProfile/SitterProfile.js
@@ -0,0 +1,31 @@
+import React, { useState } from "react";
+import AboutMeProfile from "./aboutMeProfile/AboutMeProfile";
+import BookSitter from "./bookSitter/BookSitter";
+import axios from "axios";
+import { authService } from "../../services/auth.service";
+import "./SitterProfile.css";
+function SitterProfile(props) {
+  const [profile, setProfile] = useState({});
+  const [initialized, setInitialized] = useState(false);
+  const sitterProfileId = props.profileId;
+
+  async function getProfile(profileId) {
+    axios
+      .get(`/profile/${profileId}`, authService.authHeader())
+      .then((res) => setProfile(res.data));
+  }
+  if (!initialized) {
+    getProfile(sitterProfileId);
+    setInitialized(true);
+  }
+
+  //console.log(profile);
+  return (
+    <div class="flex-container-sitter">
+      {initialized && <AboutMeProfile profile={profile} />}
+      <BookSitter />
+    </div>
+  );
+}
+
+export default SitterProfile;
---
+++ client/src/Components/sitterProfile/aboutMeProfile/AboutMeProfile.js
@@ -0,0 +1,50 @@
+import React from "react";
+import { Paper, Avatar } from "@material-ui/core";
+import "./AboutMeProfile.css";
+import { makeStyles } from "@material-ui/core/styles";
+import { Alert } from "@material-ui/lab";
+const useStyles = makeStyles({
+  profilePic: {
+    width: "140px",
+    height: "140px",
+    alignContent: "center",
+  },
+});
+
+function AboutMeProfile(props) {
+  const classes = useStyles();
+  const profile = props.profile;
+  console.log(props.profile);
+  return (
+    <div class="paper">
+      <Paper elevation={2} square={false}>
+        <img
+          class="cover"
+          alt="coverPhoto"
+          src="https://www.allstate.com/resources/Allstate/images/tools-and-resources/home/exterior-house-with-porch_Thinkstock_680x402.jpg"
+        ></img>
+
+        <div className="profile-picture">
+          <Avatar className={classes.profilePic} src={profile.profilePic} />
+        </div>
+        <div className="info">
+          <h1>
+            {profile.firstName} {profile.lastName}
+          </h1>
+        </div>
+        <div className="about-me">
+          <h2>About me</h2>
+          <br></br>
+          {profile.about} Lorem ipsum dolor sit amet consectetur adipisicing
+          elit. Dignissimos cum, placeat iusto a eius cupiditate saepe soluta
+          ipsum, cumque similique iste atque dolorem velit inventore! Cum
+          aperiam amet quidem facilis perspiciatis. Tempore natus enim tenetur
+          rerum eligendi accusantium atque, inventore aspernatur commodi
+          voluptatem aliquid impedit. Provident quis at quaerat enim!
+        </div>
+      </Paper>
+    </div>
+  );
+}
+
+export default AboutMeProfile;
