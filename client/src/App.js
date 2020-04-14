bfa198f027a56ecbff360b89a7ba4c7282973e10
Din Lanh <langdinhieu@gmail.com>
2020-04-14 16:34:43 -0400

    clean

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import { MuiPickersUtilsProvider } from "@material-ui/pickers";
 import DateFnsUtils from "@date-io/date-fns";
 import { MuiThemeProvider } from "@material-ui/core";
--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import { withStyles, CssBaseline, Grid } from "@material-ui/core";
 
 import Navbar from "../Components/shared/Navbar";
