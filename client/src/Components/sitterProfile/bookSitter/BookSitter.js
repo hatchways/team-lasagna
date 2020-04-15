d1db752985fed590bdd5bdeada1b54bb4379b63b
Frank Xie <frank.xie@mail.utoronto.ca>
2020-04-14 22:21:59 -0400

    Update client/src/Components/sitterProfile/bookSitter/BookSitter.js
    Co-Authored-By: Bonnie Li <bonnie@hatchways.io>

--- client/src/Components/sitterProfile/bookSitter/BookSitter.js
+++ client/src/Components/sitterProfile/bookSitter/BookSitter.js
@@ -1,5 +1,5 @@
 import "date-fns";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { useForm } from "react-hook-form";
 import { Paper } from "@material-ui/core";
