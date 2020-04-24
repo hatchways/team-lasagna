60038edabe1b93b1eeaa0a7435f91ffe3474890a
Gbolaga Famodun <biyibg@yahoo.co.uk>
2020-04-24 10:29:54 -0400

    dealing with bugs continu

--- client/src/Components/Bookings/UpcomingJobs.js
+++ client/src/Components/Bookings/UpcomingJobs.js
@@ -98,7 +98,7 @@ export default function UpcomingJobs() {
       setProcessing(false);
       setSuccess(true);
     } catch (error) {
-      alert('error')
+      //alert('error')
       setSuccess(false);
       setProcessing(false);
       setError(true);
